850951b1171179ccf2634aa83dc3fb206b25f7e8
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-06-16 11:44:09 +0300

    dynamic route added with params

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import PageThreadShow from "@/components/PageThreadShow.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/thread/:id",
+    name: "ThreadShow",
+    component: PageThreadShow,
+    props: true,
   },
 ];
 
