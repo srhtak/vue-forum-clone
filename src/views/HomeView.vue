b6c3900b1d58d9cd793a29cb4203f4846f5c8bb3
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-06-16 11:31:34 +0300

    styling vue components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,11 @@
+<script setup>
+import PageHome from "../components/PageHome.vue";
+</script>
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <PageHome />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style lang="scss" scoped></style>
